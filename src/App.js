75a441f51981334836194b71eb8f6e4c8dd68a22
Francisco Vergara Espinoza <neo_ca@live.com.mx>
2021-03-15 15:42:06 -0600

    feat: code coverage added in the ci

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-                  Learn React (KIMBO SLICE the best pet)
+                  Learn React (KIMBO SLICE the best pet) - Production 001
         </a>
       </header>
     </div>
