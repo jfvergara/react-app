de43970bc1739a9abca4f0ebc411ad8d6647c7f9
Francisco Vergara Espinoza <neo_ca@live.com.mx>
2021-03-15 13:07:52 -0600

    fix: the artifacts were fixed

--- release.config.js
+++ release.config.js
@@ -3,7 +3,15 @@ module.exports = {
     repositoryUrl: "https://github.com/jfvergara/react-app",
     plugins: [
         "@semantic-release/commit-analyzer", //It decides what is going to be the next version
-        "@semantic-release/release-notes-generator", // It will generate release notes for our release based on the commit messages        
-        "@semantic-release/github"  //it will create the Github release
+        "@semantic-release/release-notes-generator", // It will generate release notes for our release based on the commit messages      
+        [
+            "@semantic-release/github",  //it will create the Github release
+            {
+                assets:[
+                    { path: "build.zip", label: "Build" },
+                    { path: "coverage.zip", label: "Coverage" }
+                ]
+            }
+        ]  
     ] 
-}
+};
